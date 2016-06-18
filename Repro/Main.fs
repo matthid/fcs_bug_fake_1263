ec1fb0e78a2fc3beca3705ac7ebc8e7b47482176
Matthias Dittrich <matthi.d@gmail.com>
2016-06-18 19:41:08 +0200

    remove old code

--- Repro/Main.fs
+++ Repro/Main.fs
@@ -7,7 +7,6 @@ open System.Reflection
 //Environment.CurrentDirectory <- @"C:\PROJ\FSharp.Compiler.Service_585\bin"
 let scriptFilePath = Path.GetFullPath @"myscript.fsx"
 
-//let someLib = @"C:\PROJ\FAKE\build\FakeLib.dll" 
 let someLib = Path.GetFullPath @"bin/lib/LibraryUsingTypeprovider.dll"
 File.WriteAllText(scriptFilePath, @"
 #r """ + someLib.Replace(@"\", @"\\") + @"""
@@ -15,19 +14,9 @@ open System
 Console.WriteLine(""TEST_OUTPUT"")");
 open Microsoft.FSharp.Compiler.Interactive.Shell
 
-//let fakeLib = typeof<Fake.TraceListener.ITraceListener>.Assembly.Location
-//File.WriteAllText(scriptFilePath, @"
-//#r """ + fakeLib.Replace(@"\", @"\\") + @"""
-//open Fake
-//traceFAKE ""TEST_FAKE_OUTPUT""");
-//
-//let execScript s =
-//  Fake.FSIHelper.executeBuildScriptWithArgsAndFsiArgsAndReturnMessages s [||] [||] false
-
 [<EntryPoint>]
 let main args =
   printfn "%s" LibraryUsingTypeprovider.OtherStuff.t
-  //execScript scriptFilePath |> printfn "%A"
   let fsiConfig = FsiEvaluationSession.GetDefaultConfiguration()
   let fsiArgs = [| @"C:\fsi.exe" |]
   let inStream = new StringReader("")
